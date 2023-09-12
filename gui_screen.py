7621235ddb6182e2963c7229dafcd01bd40bd8df
tejasva-087 <khandelwaltejasva@gmail.com>
2023-09-12 16:57:28 +0530

    Done with splash screen functionality remains

--- gui_screen.py
+++ gui_screen.py
@@ -1,18 +1,14 @@
 from PIL import ImageTk
 import ttkbootstrap as ttk
 from tkinter.font import Font
+from datetime import date
 
 
 def splash_screen():
+    # Basic color and styling
+    font_color = '#fff'
+    font_family = 'Nueva Std Cond'
     root = ttk.Window()
-    # Font for the hero text
-    big_font_color = '#333'
-    big_font = Font(
-        family='Impact',
-        size=100,
-        weight="normal"
-    )
-    # Font for hero side text
 
     # Making it non resizable
     root.resizable(False, False)
@@ -31,6 +27,40 @@ def splash_screen():
     # Setting image in canvas
     bg_canvas.create_image(0, 0, image=bg, anchor='nw')
 
+    # ========================================
+    """Hero text"""
+    # ========================================
+    # Fonts for text
+    big_font = Font(
+        family=font_family,
+        size=120,
+        
+    )
+
+    small_font = Font(
+        family=font_family,
+        size=32
+    )
+    # Setting up text
+    bg_canvas.create_text(300, 150, text='Mark up', font=big_font, fill=font_color)
+    bg_canvas.create_text(180, 220, text='Edition: 2.0', font=small_font, fill=font_color)
+
+    # ========================================
+    """footer icon"""
+    # ========================================
+    # Image variable
+    footer_icon = ImageTk.PhotoImage(file=r'img/mark-up-logo.jpg')
+    # Image set up
+    bg_canvas.create_image(135, 450, image=footer_icon)
+
+    # ========================================
+    """Footer text"""
+    # ========================================
+    big_font = Font(
+        family=font_family,
+        size=18
+    )
+    bg_canvas.create_text(273, 500, text=f'Created by Tejasva Khandelwal. All rights reserved {date.today().year}')
     root.mainloop()
 
 
