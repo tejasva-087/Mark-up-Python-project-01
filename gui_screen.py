4f1662b34f7a50350e6fb5c96044bb86997038a8
Tejasva Khandelwal <135218407+tejasva-087@users.noreply.github.com>
2023-09-12 15:03:21 +0530

    Update gui_screen.py

--- gui_screen.py
+++ gui_screen.py
@@ -1,6 +1,40 @@
-import tkinter as tk
+from PIL import ImageTk
+import ttkbootstrap as ttk
+from tkinter.font import Font
 
-root = tk.Tk()
-root.title('Mark up')
-root.geometry('1200x700')
-root.mainloop()
+
+def splash_screen():
+    root = ttk.Window()
+    # Font for the hero text
+    big_font_color = '#fff'
+    big_font = Font(
+        family='Impact',
+        size=100,
+        weight="normal"
+    )
+
+    # Making it non resizable
+    root.resizable(False, False)
+
+    # Making the window spawn in middle
+    root.geometry(f'950x600+{int((root.winfo_screenwidth() / 2) - 500)}+{int((root.winfo_screenheight() / 2) - 300)}')
+
+    # ========================================
+    """Setting the background of the window"""
+    # ========================================
+    # Image variable
+    bg = ImageTk.PhotoImage(file=r'img/background.png')
+    # Canvas Image holder
+    bg_canvas = ttk.Canvas(root, width=950, height=600)
+    bg_canvas.pack(fill='both', expand=True)
+    # Setting image in canvas
+    bg_canvas.create_image(0, 0, image=bg, anchor='nw')
+
+    # Creating text in canvas
+    bg_canvas.create_text(300, 150, text='Mark Up', font=big_font, fill=big_font_color)
+    bg_canvas.create_text(165, 300, text='2.0', font=big_font, fill=big_font_color)
+
+    root.mainloop()
+
+
+splash_screen()
